d8e0fcad963d28df4acddb5e5b2d934b51df1367
Ian <ian01story@gmail.com>
2022-12-07 11:54:16 +0100

    feat(gs-13): Code diff count statistics entity implementation

--- src/domain/entities/statistics/code-diff-count-statistics.entity.ts
+++ src/domain/entities/statistics/code-diff-count-statistics.entity.ts
@@ -2,8 +2,28 @@ import { StatisticsInterface } from "@/domain/interfaces/entities";
 import { OrganizationEntity } from "@/domain/entities";
 
 class CodeDiffCountStatisticsEntity implements StatisticsInterface {
-  calculate(organization: OrganizationEntity) {
-    // ...
+  calculate(organization: OrganizationEntity): Record<string, number> {
+    const repositories = organization.getRepositories();
+    const users = organization.getUsers();
+    const statistics = {};
+
+    for (const user of users) {
+      let codeDiffCount = 0;
+      for (const repository of repositories) {
+        const repositoryPullRequests = repository.getPullRequests();
+        for (const pullRequest of repositoryPullRequests) {
+          const pullRequestCommits = pullRequest.getCommits();
+          for (const commit of pullRequestCommits) {
+            if (commit.getAuthorLogin() === user.getId()) {
+              codeDiffCount += commit.getCodeDiffCount();
+            }
+          }
+        }
+      }
+      statistics[user.getId()] = codeDiffCount;
+    }
+
+    return statistics;
   }
 }
 
