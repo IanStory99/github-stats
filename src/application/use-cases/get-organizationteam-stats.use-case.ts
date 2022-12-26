4468653ea903ec102a2f6322e6ab87a2fc225363
Diego <root.chile@gmail.com>
2022-12-26 18:47:10 +0100

    feat(gs-16): add filter by team use case

---
+++ src/application/use-cases/get-organizationteam-stats.use-case.ts
@@ -0,0 +1,60 @@
+import { OrganizationEntity, UserStatisticsEntity } from "@/domain/entities";
+import {
+  OrganizationInputDto
+} from "@/application/dtos";
+import {
+  OrganizationServiceInterface,
+  UserStatisticsServiceInterface,
+  FormattingServiceInterface
+} from "@/domain/interfaces/services";
+import OrganizationTeamInputDto from "../dtos/organizationteam-input.dto";
+
+class GetOrganizationTeamStatsUseCase {
+  private organizationService: OrganizationServiceInterface;
+  private userStatisticsService: UserStatisticsServiceInterface;
+  private formattingService: FormattingServiceInterface;
+
+  constructor(
+    organizationService: OrganizationServiceInterface,
+    userStatisticsService: UserStatisticsServiceInterface,
+    formattingService: FormattingServiceInterface
+  ) {
+    this.organizationService = organizationService;
+    this.userStatisticsService = userStatisticsService;
+    this.formattingService = formattingService;
+  }
+
+  async execute(organizationInputDto: OrganizationTeamInputDto) {
+    const organization = await this.organizationService.getOrganizationByIdFilteredByTeamSlug(
+      organizationInputDto
+    );
+    const organizationUsersStatistics = this.userStatisticsService.getOrganizationUsersStatistics(
+      organization
+    );
+    const statsJSON = this.buildStatsJSON(organization, organizationUsersStatistics);
+    const formattedStatistics = this.formattingService.format(statsJSON);
+    console.table(statsJSON);
+    formattedStatistics.saveToFile();
+
+    return formattedStatistics;
+  }
+
+  private buildStatsJSON(organization: OrganizationEntity, organizationUsersStatistics: UserStatisticsEntity[]): object {
+    const statsJSON = [];
+    for (const team of organization.getTeams()) {
+      for (const user of team.getMembers()) {
+        const userStatistics = organizationUsersStatistics.find(
+          (userStatistics) => userStatistics.getUserId() === user.getId()
+        );
+        statsJSON.push({
+          username: user.getLogin(),
+          team: team.getName(),
+          stats: userStatistics.getStatistics()
+        });
+      }
+    }
+    return statsJSON;
+  }
+}
+
+export default GetOrganizationTeamStatsUseCase;
--- src/domain/interfaces/services/organization-service.interface.ts
+++ src/domain/interfaces/services/organization-service.interface.ts
@@ -1,8 +1,10 @@
 import { OrganizationInputDto } from '@/application/dtos';
+import OrganizationTeamInputDto from '@/application/dtos/organizationteam-input.dto';
 import OrganizationEntity from '@/domain/entities/organization.entity';
 
 interface OrganizationServiceInterface {
   getOrganizationById(organizationDTO: OrganizationInputDto): Promise<OrganizationEntity>;
+  getOrganizationByIdFilteredByTeamSlug(organizationTeamDTO: OrganizationTeamInputDto): Promise<OrganizationEntity>;
 }
 
 export default OrganizationServiceInterface;
